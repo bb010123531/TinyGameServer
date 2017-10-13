3939cfdb9a641fb7b8e8218587741c86cd6cd3cf
unknown <t-wangchunbo-zy@service.360ityun.com>
2017-10-13 10:29:45 +0800

    first commit

---
+++ src/main/java/tiny/gameserver/App.java
@@ -0,0 +1,13 @@
+package tiny.gameserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git!" );
+    }
+}
